d28d7535e87201d6d59ecfc73c00403b70b1ce53
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-11 18:57:52 +0530

    update

---
+++ backend/app/models/post.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer,ForeignKey
+from .base import Base
+
+
+class Post(Base):
+
+    __tablename__="posts"
+
+    id=Column(Integer,primary_key=True,Index=True)
+    title=Column(String(250),nullable=False)
+    image=Column(String(240))
+    author_id = Column(Integer,ForeignKey("users.id"))
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,11 @@
-from sqlalchemy import column,Integer,String
+from sqlalchemy import Column,Integer,String
 
 from .base import Base
 
 class User(Base):
 
     __tablename__="users"
+    id=Column(Integer,primary_key=True,Index=True)
+    username=Column(String(100),nullable=False)
+    email=Column(String(150),unique=True,nullable=False)
+    password=Column(String(50),default="user")
