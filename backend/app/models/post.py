b717e7be8d8644c431194e5024caa82748e9e983
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-11 20:00:42 +0530

    run

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -6,7 +6,7 @@ class Post(Base):
 
     __tablename__="posts"
 
-    id=Column(Integer,primary_key=True,Index=True)
+    id=Column(Integer,primary_key=True,index=True)
     title=Column(String(250),nullable=False)
     image=Column(String(240))
     author_id = Column(Integer,ForeignKey("users.id"))
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,7 +5,7 @@ from .base import Base
 class User(Base):
 
     __tablename__="users"
-    id=Column(Integer,primary_key=True,Index=True)
+    id=Column(Integer,primary_key=True,index=True)
     username=Column(String(100),nullable=False)
     email=Column(String(150),unique=True,nullable=False)
     password=Column(String(50),default="user")
