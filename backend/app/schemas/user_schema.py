6f1900da2b716876e5dac91a6844353c8923f420
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-11 22:40:12 +0530

    usercreate

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,1 +1,11 @@
-p
+from pydantic import BaseModel,EmailStr
+
+class UserCreate(BaseModel):
+    username:str
+    email:str
+    password:str
+
+
+class UserLogin(BaseModel):
+    email:str
+    password:str
